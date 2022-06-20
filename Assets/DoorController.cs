123df21d6ce681548d6d8bd859ac7d517e5fb588
marun <norihiro.maruyama@gmail.com>
2022-06-20 18:56:41 +0900

    implemented door

--- Assets/DoorController.cs
+++ Assets/DoorController.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    
+    public SerialHandler serialHandler;    
     // Start is called before the first frame update
+
+    private float doorSerialValueClose = 1112;
+    private float doorSerialValueOpen = 722;
     void Start()
     {
-        
+        serialHandler.OnDataReceived += OnDataReceived;    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnDataReceived(string message)
+    {
+        var data = message.Split(
+                new string[] { "\n" }, System.StringSplitOptions.None);
+        try
+        {
+            float v = float.Parse(data[0]);
+            Debug.Log("###Door###");
+            Debug.Log(v);
+            Debug.Log("##########");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);//エラーを表示
+        }
     }
 }
--- Assets/PassthroughRoomController.cs
+++ Assets/PassthroughRoomController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PassthroughRoomController : MonoBehaviour
 {
+    public bool useDoor = true;
     [Range(-90.0f, 90.0f)]
     public float doorAngle = 0.0f;
     public GameObject door;
@@ -12,8 +13,15 @@ public class PassthroughRoomController : MonoBehaviour
     public GameObject wall;
     public GameObject headAnchor;
 
+    public SerialHandler serialHandler;    
+    // Start is called before the first frame update
+
+    private float doorSerialValueClose = 1012;
+    private float doorSerialValueOpen = 722;
+
     void Start()
     {
+        serialHandler.OnDataReceived += onSerialDataReceived;
     }
 
     void Update()
@@ -21,6 +29,7 @@ public class PassthroughRoomController : MonoBehaviour
         const float HEAD_SIZE_OFFSET = 0.1f;
         Vector3 headFromDoor = Quaternion.Inverse(transform.rotation) * (headAnchor.transform.position - transform.position);
         wall.SetActive(headFromDoor.z < HEAD_SIZE_OFFSET);
+        if (!useDoor) { doorAngle = -90; }
         DoorHinge.transform.rotation = transform.rotation * Quaternion.Euler(0, doorAngle, 0);
     }
 
@@ -42,4 +51,23 @@ public class PassthroughRoomController : MonoBehaviour
         // GameObject door = GameObject.Find("Door");
         door.transform.localScale = new Vector3(w, h, 0.01f);
     } 
+
+    private void onSerialDataReceived(string message)
+    {
+        var data = message.Split(new string[] { "\n" }, System.StringSplitOptions.None);
+        if (!useDoor) { return; }
+        try
+        {
+            float v = float.Parse(data[0]);
+            doorAngle = -90 + 90 * (v - 722) / (1012 - 722);
+            Debug.Log("###Door###");
+            Debug.Log(v);
+            Debug.Log(doorAngle);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);//エラーを表示
+        }
+    }
+
 }
